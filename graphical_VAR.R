a045f3adfafc92ebeb5ea17d672c3d3bcc31194d
Niamh Sheehan <sheehan3@tcd.ie>
2022-06-21 17:30:12 +0200

    Update graphical_VAR.R

--- graphical_VAR.R
+++ graphical_VAR.R
@@ -1,38 +1,48 @@
 library('graphicalVAR')
 
-df <- read.csv('C:/Users/35387/Dropbox/Documents/Brownlees/final project/git/psych-networks-brownlees/ESM_using.csv')
+df <- read.csv('C:/Users/35387/Dropbox/Documents/Brownlees/final project/git/psych-networks-brownlees/ESM_using.csv', stringsAsFactors = TRUE)
 
-vars = c('Control') # 'Sad', 'Intrusion', 'Encourage', 
+df$day_var = as.factor(df$day_var)
+df$beep_var = as.factor(df$beep_var)
+df$id_var = as.factor(df$id_var)
+
+vars = c('Control', 'Sad', 'Intrusion', 'Encourage', 
          'FunThings', 'IgotThis', 'Alone', 'Thoughts', 
-         'Appointments', 'Laying', 'Qualitysleep',
-         'Out', 'Eaten', 'Avoid', 'Useful', 'Enjoy', 
-         'Pleasant')
+         'Appointments', 'Laying',
+         'Out', 'Eaten', 'Avoid', 'Useful', 'Enjoy')
 
 keep = c('Control', 'Sad', 'Intrusion', 'Encourage', 
          'FunThings', 'IgotThis', 'Alone', 'Thoughts', 
          'Appointments', 'Laying', 'Out',
-         'Eaten', 'Avoid', 'Useful', 'Enjoy')
-         #'day_var', 'beep_var', 'id_var')
+         'Eaten', 'Avoid', 'Useful', 'Enjoy',
+         'day_var', 'beep_var', 'id_var')
 
 
 fresh <- df[keep]
 fresh_sub <- fresh[1:25,]
 
-dayvar = paste(df[1:25,]$day_var)
-beepvar = paste(df[1:25,]$beep_var)
-idvar = paste(df[1:25,]$id_var)
+dayvar = df[1:25,]$day_var
+beepvar = df[1:25,]$beep_var
+idvar = df[1:25,]$id_var
 
+lambda_beta = c(0.01)
+lambda_kappa = c(0.01)
 
 
 graphicalVAR(fresh_sub, nLambda = 50, verbose = TRUE, 
              gamma = 0.5, 
              scale = TRUE, 
+             lambda_beta,
+             lambda_kappa,
+             #regularize_mat_beta, 
+             #regularize_mat_kappa,
              maxit.in = 100, 
              maxit.out = 100, 
              deleteMissings = TRUE, 
-             penalize.diagonal = TRUE, 
+             penalize.diagonal = TRUE,
              lambda_min_kappa = 0.05,
              lambda_min_beta = 0.05, 
+             mimic = c("current","0.1.2","0.1.4","0.1.5","0.2"),
              vars,
              beepvar, 
              dayvar, 
@@ -40,40 +50,3 @@ graphicalVAR(fresh_sub, nLambda = 50, verbose = TRUE,
              lags = 1, 
              centerWithin = TRUE,
              likelihood = c("unpenalized", "penalized"))
-
-
-#trying stuff
-no_time <- subset(df, select=-c(SentAt,StartedAt))
-
-trial1 = c('Control', 'Sad', 'Intrusion', 'Encourage', 
-         'FunThings', 'IgotThis', 'Alone', 'Thoughts', 
-         'Appointments', 'Laying', 'Qualitysleep',
-         'Out', 'Eaten', 'Avoid', 'Useful', 'Enjoy', 
-         'Pleasant')
-bigger <- df[trial1]
-
-
-trial2 <- c("Control", "Sad")
-small <- df[trial2]
-
-
-graphicalVAR(df, nLambda = 50, verbose = TRUE, 
-             gamma = 0.5, 
-             scale = TRUE, 
-             maxit.in = 100, 
-             maxit.out = 100, 
-             deleteMissings = TRUE, 
-             penalize.diagonal = TRUE, 
-             lambda_min_kappa = 0.05,
-             lambda_min_beta = lambda_min_kappa, 
-             vars,
-             beepvar, 
-             dayvar, 
-             idvar, 
-             lags = 1, 
-             centerWithin = TRUE,
-             likelihood = c("unpenalized", "penalized"))
-
-graphicalVAR(bigger, gamma = 0.5, nLambda = 5)
-
-
